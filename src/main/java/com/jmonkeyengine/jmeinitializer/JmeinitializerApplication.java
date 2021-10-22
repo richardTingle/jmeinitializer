c8b5b93884608d073ef2ce881c8e5c4c7e5ebc00
Richard Tingle <6330028+richardTingle@users.noreply.github.com>
2021-10-22 18:10:23 +0100

    Initial repository setup

---
+++ src/main/java/com/jmonkeyengine/jmeinitializer/JmeinitializerApplication.java
@@ -0,0 +1,13 @@
+package com.jmonkeyengine.jmeinitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeinitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmeinitializerApplication.class, args);
+	}
+
+}
