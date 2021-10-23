772fc8e58395040e6c7480ea8d710dcf79e6869f
Richard Tingle <6330028+richardTingle@users.noreply.github.com>
2021-10-23 10:00:39 +0100

    Add the missing main game file

---
+++ src/main/resources/jmetemplate/src/main/java/[GAME_PACKAGE_FOLDER]/[GAME_NAME].java
@@ -0,0 +1,42 @@
+package [GAME_PACKAGE];
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.renderer.RenderManager;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+
+/**
+ * This is the Main Class of your Game. It should boot up your game and do initial initialisation
+ * Move your Logic into AppStates or Controls or other java classes
+ */
+public class [GAME_NAME] extends SimpleApplication {
+
+    public static void main(String[] args) {
+        [GAME_NAME] app = new [GAME_NAME]();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1);
+        Geometry geom = new Geometry("Box", b);
+
+        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+
+        rootNode.attachChild(geom);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //this method will be called every game tick and can be used to make updates
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //add render code here (if any)
+    }
+}
