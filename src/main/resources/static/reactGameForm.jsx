df0438ee7b8591a6b2dc4342be0f1f54ccefe18a
Richard Tingle <6330028+richardTingle@users.noreply.github.com>
2021-10-31 11:12:48 +0000

    Add jmonkey styles to the initializer

--- src/main/resources/static/reactGameForm.jsx
+++ src/main/resources/static/reactGameForm.jsx
@@ -233,12 +233,12 @@ class ReactGameForm extends React.Component {
             <div className="form-group">
                 <label htmlFor="gameName"><b>Game Name</b></label>
                 <input className="form-control" value={this.state.gameName} onChange={this.handleSetGameName} id="gameName" aria-describedby="gameNameHelp" placeholder="e.g. Asteroids" required/>
-                <small id="gameNameHelp" className="form-text text-muted">This will be the name of your project. Try to keep to english letters as it will also be used in your java source</small>
+                <small id="gameNameHelp" className="form-text">This will be the name of your project. Try to keep to english letters as it will also be used in your java source</small>
             </div>
             <div className="form-group">
                 <label htmlFor="gamePackage"><b>Package Name (Optional)</b></label>
                 <input className="form-control" value={this.state.package} onChange={this.handleSetPackage} id="gamePackage" aria-describedby="gamePackageHelp" placeholder="e.g. com.mycompany"/>
-                <small id="gameNameHelp" className="form-text text-muted">A package name keeps your classes unique. If you have a website it's traditionally the website backwards (all lower case). So myamazinggame.co.uk would become uk.co.myamazinggame. If you don't have a website choose something like that, or just leave it blank</small>
+                <small id="gameNameHelp" className="form-text">A package name keeps your classes unique. If you have a website it's traditionally the website backwards (all lower case). So myamazinggame.co.uk would become uk.co.myamazinggame. If you don't have a website choose something like that, or just leave it blank</small>
             </div>
 
             <h2>
@@ -250,7 +250,6 @@ class ReactGameForm extends React.Component {
             <div className="alert alert-secondary" role="alert">
                 Don't worry if you're not sure about libraries, you can always add more later
             </div>
-
             <h2>
                 Additional JME libraries
             </h2>
