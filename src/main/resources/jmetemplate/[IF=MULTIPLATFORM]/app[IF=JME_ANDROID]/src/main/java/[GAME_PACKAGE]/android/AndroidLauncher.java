af3f326158e5c125811898439618e1a78adb55a2
Richard Tingle <6330028+richardTingle@users.noreply.github.com>
2022-01-09 13:24:53 +0000

    Correct the android build version and the imports for the android launcher

--- src/main/resources/jmetemplate/[IF=MULTIPLATFORM]/app[IF=JME_ANDROID]/src/main/java/[GAME_PACKAGE]/android/AndroidLauncher.java
+++ src/main/resources/jmetemplate/[IF=MULTIPLATFORM]/app[IF=JME_ANDROID]/src/main/java/[GAME_PACKAGE]/android/AndroidLauncher.java
@@ -1,19 +1,8 @@
 package [GAME_PACKAGE].android;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.View;
+import com.jme3.app.AndroidHarness;
+import [GAME_PACKAGE].game.[GAME_NAME];
 
-import com.google.android.material.snackbar.BaseTransientBottomBar;
-import com.google.android.material.snackbar.Snackbar;
-import com.jme3.app.LegacyApplication;
-import com.jme3.app.jmeSurfaceView.JmeSurfaceView;
-import com.jme3.app.jmeSurfaceView.OnExceptionThrown;
-import com.jme3.app.jmeSurfaceView.OnRendererCompleted;
-import myGame.Game;
-import com.jme3.system.AppSettings;
 
 public class AndroidLauncher extends AndroidHarness {
 
