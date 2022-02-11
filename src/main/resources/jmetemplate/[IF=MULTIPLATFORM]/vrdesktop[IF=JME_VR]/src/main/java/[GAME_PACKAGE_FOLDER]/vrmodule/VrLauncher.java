1a9a57aa63bfbd12fae0a38f88df8379f665c333
Richard Tingle <6330028+richardTingle@users.noreply.github.com>
2022-02-11 11:26:47 +0000

    Add VR to the multi-module template

---
+++ src/main/resources/jmetemplate/[IF=MULTIPLATFORM]/vrdesktop[IF=JME_VR]/src/main/java/[GAME_PACKAGE_FOLDER]/vrmodule/VrLauncher.java
@@ -0,0 +1,26 @@
+package [GAME_PACKAGE].vrmodule;
+
+import [GAME_PACKAGE].game.[GAME_NAME];
+import com.jme3.system.AppSettings;
+
+/**
+ * Used to launch a jme application in desktop VR environment
+ *
+ */
+public class VrLauncher {
+    public static void main(String[] args) {
+        AppSettings settings = new AppSettings(true);
+        settings.put(VRConstants.SETTING_VRAPI, VRConstants.SETTING_VRAPI_OPENVR_LWJGL_VALUE);
+        VREnvironment env = new VREnvironment(settings);
+        env.initialize();
+        if (env.isInitialized()){
+            VRAppState vrAppState = new VRAppState(settings, env);
+
+            [GAME_NAME] app = new [GAME_NAME](vrAppState);
+            app.setLostFocusBehavior(LostFocusBehavior.Disabled);
+            app.setSettings(settings);
+            app.setShowSettings(false);
+            app.start();
+        }
+    }
+}
